aedd4cd36e9e8e10d67e493c2c8d74981106755f
sxr8799 <106579384+sxr8799@users.noreply.github.com>
2022-06-17 11:35:22 +0400

    Add files via upload
    Add disk full check to all_checks.py

---
+++ all_checks.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+def check_reboot():
+    """Returns True if computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def check_disk_usage(disk, min_absolute, min_percent):
+    """Returns True if there is enough disk space, fale otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
+
+# Check for at least 2 GB and 10% free
+if not check_disk_usage("/", 2, 10):
+    print("ERROR: Not enough disk space.")
+    sys.exit(1)
+
+print("Everything ok.")
+sys.exit(0)
+
+def main():
+    if check_reboot():
+        print("Pending Reboot")
+        sys.exit(1)
+    print("Everything ok.")
+    sys.exit(0)
+
+main()
